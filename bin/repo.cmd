3817640c61e1a0067cf56c79ee7d5e2b266f66ef
Titan Lee <titan.lee@amd.com>
2021-01-08 17:26:57 +0800

    1. Update ctags.exe, fzf.exe 2. Add repo.

---
+++ bin/repo.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+REM This is a batch file to run the repo python script and can be put in the path.
+REM Modify your own local copy as needed
+REM You should have Python 3.8 or above installed and in the path.
+
+@python c:\utility_tools\bin\repo %*
